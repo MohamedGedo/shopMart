af2aacf771c54886ae3a2944537a343c816ecc03
Mohamed Sayed <ms7147814@gmail.com>
2026-02-16 23:19:42 +0200

    ُTest

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -6,7 +6,7 @@ export default function Login() {
       <h2>Login</h2>
       <LoginForm />
       <p>
-        If you don't have account, please{" "}
+        If you donot have account, please{" "}
         <a className="text-blue-600 underline" href="/register">
           SignUp
         </a>{" "}
